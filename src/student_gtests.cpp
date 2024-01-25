8830a1ea2afcd88aefa0a27513497b4fa051034e
Richard Zhang <richarzz@uci.edu>
2024-01-24 23:11:32 -0800

    starter

--- src/student_gtests.cpp
+++ src/student_gtests.cpp
@@ -53,13 +53,10 @@ TEST(WordCount, CountWords) {
     auto word_counts3 = count_words(test_input3, stopwords1);
     EXPECT_TRUE(word_counts3.empty());
 
-	std::istringstream test_input4("apple! cherry? 123 apple; HELLO");
-    std::set<std::string> stopwords4 = {"hello"};
-    auto word_counts4 = count_words(test_input4, stopwords4);
-    EXPECT_EQ(word_counts4["apple"], 2);
-    EXPECT_EQ(word_counts4["cherry"], 1);
-    EXPECT_EQ(word_counts4.count("123"), 0); // Numbers should not be counted
-    EXPECT_EQ(word_counts4.count("hello"), 0);
+	std::istringstream test_input4("apple! cherry?");
+    auto word_counts4 = count_words(test_input4, {});
+    EXPECT_EQ(word_counts4["apple!"], 1);
+    EXPECT_EQ(word_counts4["cherry?"], 1);
 }
 
 TEST(WordCount, OutputWordCounts) {
