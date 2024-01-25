3b69c0c337e9c10822d2bb297cbae7038f982c71
Richard Zhang <richarzz@uci.edu>
2024-01-24 22:01:41 -0800

    starter

--- src/student_gtests.cpp
+++ src/student_gtests.cpp
@@ -53,7 +53,9 @@ TEST(WordCount, CountWords) {
     auto word_counts3 = count_words(test_input3, stopwords1);
     EXPECT_TRUE(word_counts3.empty());
 
-   
+	std::istringstream test_input4("apple! cherry? apple;");
+    auto word_counts4 = count_words(test_input4, {});
+    EXPECT_EQ(word_counts4["cherry?"], 1);
 }
 
 TEST(WordCount, OutputWordCounts) {
