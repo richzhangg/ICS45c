7954e61709543119cbc8247d140604a85d232d44
Richard Zhang <richarzz@uci.edu>
2024-02-07 19:52:56 -0800

    “something”

---
+++ src/string.cpp
@@ -0,0 +1,267 @@
+#include "string.hpp"
+#include <cstring>
+#include <iostream>
+#include <utility>
+
+char* String::strdup(const char* src) {
+    if (!src) {
+        return new char[1]{ '\0' };
+    }
+    size_t length = std::strlen(src) + 1; 
+    char* newStr = new char[length];
+    std::strcpy(newStr, src);
+    return newStr;
+}
+
+
+String::String(const char* s) : buf(strdup(s)) {}
+
+
+String::String(const String& s) : buf(strdup(s.buf)) {}
+
+
+String::~String() {
+    delete[] buf;
+}
+
+
+void String::swap(String& s) {
+    std::swap(buf, s.buf);
+}
+
+
+String& String::operator=(String s) {
+    swap(s);
+    return *this;
+}
+
+
+char& String::operator[](int index) {
+    return buf[index];
+}
+
+
+const char& String::operator[](int index) const {
+    return buf[index];
+}
+
+
+int String::size() const {
+    return std::strlen(buf);
+}
+
+
+String String::reverse() const {
+    int len = size();
+    char* reversed = new char[len + 1];
+    for (int i = 0; i < len; ++i) {
+        reversed[i] = buf[len - i - 1];
+    }
+    reversed[len] = '\0';
+    String result(reversed);
+    delete[] reversed;
+    return result;
+}
+
+
+int String::indexOf(char c) const {
+    const char* found = std::strchr(buf, c);
+    if (found) {
+        return found - buf;
+    }
+    return -1;
+}
+
+
+int String::indexOf(String s) const {
+    const char* found = std::strstr(buf, s.buf);
+    if (found) {
+        return found - buf;
+    }
+    return -1;
+}
+
+
+bool String::operator==(String s) const {
+    return std::strcmp(buf, s.buf) == 0;
+}
+
+bool String::operator!=(String s) const {
+    return !(*this == s);
+}
+
+bool String::operator<(String s) const {
+    return std::strcmp(buf, s.buf) < 0;
+}
+
+bool String::operator>(String s) const {
+    return s < *this;
+}
+
+bool String::operator<=(String s) const {
+    return !(*this > s);
+}
+
+bool String::operator>=(String s) const {
+    return !(*this < s);
+}
+
+
+String String::operator+(String s) const {
+    char* concatenated = new char[size() + s.size() + 1];
+    std::strcpy(concatenated, buf);
+    std::strcat(concatenated, s.buf);
+    String result(concatenated);
+    delete[] concatenated;
+    return result;
+}
+
+
+String& String::operator+=(String s) {
+    *this = *this + s;
+    return *this;
+}
+
+
+void String::print(std::ostream& out) const {
+    out << buf;
+}
+
+
+void String::read(std::istream& in) {
+    char buffer[1024];
+    in >> buffer;
+    delete[] buf;
+    buf = strdup(buffer);
+}
+
+
+int String::strlen(const char *s) {
+    int length = 0;
+    while (s && s[length]) {
+        ++length;
+    }
+    return length;
+}
+
+
+char *String::strcpy(char *dest, const char *src) {
+    int i = 0;
+    while (src[i]) {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+
+char* String::strdup(const char* src) {
+    int length = strlen(src) + 1;
+    char *newStr = new char[length];
+    strcpy(newStr, src);
+    return newStr;
+}
+
+
+char *String::strncpy(char *dest, const char *src, int n) {
+    int i = 0;
+    for (; src[i] && i < n; ++i) {
+        dest[i] = src[i];
+    }
+    for (; i < n; ++i) {
+        dest[i] = '\0';
+    }
+    return dest;
+}
+
+
+char *String::strcat(char *dest, const char *src) {
+    int destLen = strlen(dest);
+    int i = 0;
+    while (src[i]) {
+        dest[destLen + i] = src[i];
+        i++;
+    }
+    dest[destLen + i] = '\0';
+    return dest;
+}
+
+
+char *String::strncat(char *dest, const char *src, int n) {
+    int destLen = strlen(dest);
+    int i = 0;
+    for (; src[i] && i < n; ++i) {
+        dest[destLen + i] = src[i];
+    }
+    dest[destLen + i] = '\0';
+    return dest;
+}
+
+
+int String::strcmp(const char *left, const char *right) {
+    for (; *left && *right && *left == *right; ++left, ++right);
+    return *(const unsigned char *)left - *(const unsigned char *)right;
+}
+
+
+int String::strncmp(const char *left, const char *right, int n) {
+    for (int i = 0; i < n && *left && *right; ++i, ++left, ++right) {
+        if (*left != *right) {
+            return *(const unsigned char *)left - *(const unsigned char *)right;
+        }
+    }
+    return 0;
+}
+
+
+void String::reverse_cpy(char *dest, const char *src) {
+    int len = strlen(src);
+    for (int i = 0; i < len; ++i) {
+        dest[i] = src[len - i - 1];
+    }
+    dest[len] = '\0';
+}
+
+
+const char *String::strchr(const char *str, char c) {
+    while (*str) {
+        if (*str == c) {
+            return str;
+        }
+        ++str;
+    }
+    return nullptr;
+}
+
+
+const char *String::strstr(const char *haystack, const char *needle) {
+    if (!*needle) {
+        return haystack;
+    }
+    const char *p1;
+    const char *p2;
+    const char *p1_advance = haystack;
+    for (p1 = haystack, p2 = needle; *p1 && *p2; p1++, p2++) {
+        if (*p1 != *p2) {
+            p2 = needle
+            if (!*p1_advance) {
+                return nullptr;
+            }
+            p1 = p1_advance++;
+        }
+    }
+    return *p2 == '\0' ? p1_advance : nullptr;
+}
+
+
+std::ostream& operator<<(std::ostream& out, String s) {
+    s.print(out);
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, String& s) {
+    s.read(in);
+    return in;
+}
+
