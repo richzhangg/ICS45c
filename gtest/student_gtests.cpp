1439f69aa7c6ca6ef89bd62740f14ac15157f1a2
Richard Zhang <richarzz@uci.edu>
2024-02-07 23:08:58 -0800

    “so

--- gtest/student_gtests.cpp
+++ gtest/student_gtests.cpp
@@ -26,7 +26,7 @@ TEST(StringFunction, strcpy) {
 TEST(StringFunction, strdup) {
     char* duplicated = String::strdup("test");
     EXPECT_STREQ(duplicated, "test");
-    delete[] duplicated; // Remember to deallocate memory to prevent memory leaks
+    delete[] duplicated;
 }
 
 TEST(StringFunction, strncpy) {
@@ -54,17 +54,12 @@ TEST(StringFunction, strcmp) {
 }
 
 TEST(StringFunction, strncmp) {
-    // Strings are equal within comparison length
     EXPECT_EQ(String::strncmp("Hello", "Hello", 5), 0);
-    // Strings differ within comparison length
     EXPECT_LT(String::strncmp("Hello", "World", 5), 0);
     EXPECT_GT(String::strncmp("World", "Hello", 5), 0);
-    // Comparison length shorter than the point of difference
     EXPECT_EQ(String::strncmp("Hello", "Helicopter", 3), 0);
-    // One string is a prefix of the other
     EXPECT_LT(String::strncmp("Hello", "Hello world", 20), 0);
     EXPECT_GT(String::strncmp("Hello world", "Hello", 20), 0);
-    // Comparing with empty strings
     EXPECT_EQ(String::strncmp("", "", 5), 0);
     EXPECT_LT(String::strncmp("", "Hello", 5), 0);
     EXPECT_GT(String::strncmp("Hello", "", 5), 0);
@@ -85,17 +80,11 @@ TEST(StringFunction, strchr) {
 
 TEST(StringFunction, strstr) {
     const char* haystack = "Hello world";
-    // Substring at the start
     EXPECT_EQ(String::strstr(haystack, "Hello"), haystack);
-    // Substring in the middle
     EXPECT_EQ(String::strstr(haystack, "lo wo"), haystack + 3);
-    // Substring at the end
     EXPECT_EQ(String::strstr(haystack, "world"), haystack + 6);
-    // Substring not present
     EXPECT_EQ(String::strstr(haystack, "xyz"), nullptr);
-    // Empty substring
     EXPECT_EQ(String::strstr(haystack, ""), haystack);
-    // Substring longer than haystack
     EXPECT_EQ(String::strstr(haystack, "Hello world!"), nullptr);
 }
 
