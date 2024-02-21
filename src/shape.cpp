bf533b72ce857da7499580bef19c85ad5e2b0540
Richard Zhang <richarzz@uci.edu>
2024-02-21 02:16:30 -0800

    “something”

--- src/shape.cpp
+++ src/shape.cpp
@@ -1,10 +1,8 @@
 #include "shape.hpp"
 #include <iostream>
 
-// Implement the constructor
 Shape::Shape(Point center, std::string name) : center(center), name(std::move(name)) {}
 
-// Implement the print function
 void Shape::print(std::ostream& out) const {
     out << name << "(" << center.x << ", " << center.y << ")" << std::endl;
 }
