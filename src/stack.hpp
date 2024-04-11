8076f7b8abb73864b483fdb04bd1a61edcbd074c
Richard Zhang <richarzz@uci.edu>
2024-04-10 21:18:12 -0700

    “something”

--- src/stack.hpp
+++ src/stack.hpp
@@ -1,75 +1,59 @@
 #include <iostream>
-using namespace std;
+
 constexpr int STK_MAX = 1000;
 
-class Stack
-{
-	int _top;
-	char buf[STK_MAX];
+class Stack {
+    int _top;
+    char buf[STK_MAX];
+
 public:
 	// constructs this Stack
-	Stack() {
+    Stack() {
 		_top = -1;
 	}
 	// adds c to the top of this Stack
-	void push(char c)
-	{
-		if(!isFull()) {
+    void push(char c) {
+        if (!isFull()) {
 			_top++;
-			buf[_top] = c;
-		} 
-	}
-	// removes and returns the top character of this Stack
-	char pop()
-	{
-		if(!isEmpty()) {
-			_top--;
-			return buf[_top];
-		}
-		return '@';
-	}
+            buf[_top] = c;
+        }
+    }
+	// removes and returns the top character fo this Stack
+    char pop() {
+        if (!isEmpty()) {
+            return buf[_top--];
+        }
+        return '@';
+    }
 	// peeks at the top char and returns it without removing
-	char top()
-	{
-		if(!isEmpty()) {
-			return buf[_top];
-		}
-		return '@';
-	}
+    char top() {
+        if (!isEmpty()) {
+            return buf[_top];
+        }
+        return '@';
+    }
 	// return true if this stack is empty
-	bool isEmpty() 
-	{
-		if(_top == -1) {
-			return true;
-		} else {
-		return false;
-		}
-	}
+    bool isEmpty() {
+        return _top == -1;
+    }
 	// return true if this stack is full
-	bool isFull() 
-	{
-		if(_top == STK_MAX -1) {
-			return true;
-		} else {
-		return false;
-		}
-	}
+    bool isFull() {
+        return _top == STK_MAX - 1;
+    }
 };
 
 // pushes all the characters from string line onto the Stack stk
-void push_all(Stack & stk, const std::string &line)
-{
-	for (char c : line) {
-		stk.push(c);
-	}
+void push_all(Stack &stk, string line) {
+    for (int c = 0; c < line.size(); ++c) {
+        stk.push(line[c]);
+    }
 }
 
 // pops characters from stk and prints to cout
 // all chars will be on the same line without any extra spaces
-void pop_all(Stack & stk)
-{
-	while (!stk.isEmpty()) {
-		std::cout << stk.pop();
-	}
-	std::cout << std::endl;
+void pop_all(Stack & stk) {
+    while (!stk.isEmpty()) {
+        std::cout << stk.pop();
+    }
+    std::cout << std::endl;
 }
