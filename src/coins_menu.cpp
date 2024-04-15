3fa9ed26bb3e0cdd71615b303afe753fba104c63
Richard Zhang <richarzz@uci.edu>
2024-04-15 14:44:07 -0700

    “something”

--- src/coins_menu.cpp
+++ src/coins_menu.cpp
@@ -1,10 +1,1 @@
-#include <iostream>
 
-#include "coins.hpp"
-
-using namespace std;
-
-int main() {
-	coins_menu(std::cin, std::cout);
-	return 0;
-}
--- src/coins_simple.cpp
+++ src/coins_simple.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include "coins.hpp"
-using namespace std;
-
-constexpr int CENTS_FOR_CANDYBAR = 482;
-
-int main() {
-    Coins pocket(100, 10, 10, 100);
-    cout << "I started with " << pocket << " in my pocket." << endl;
-	Coins requiredChange = coins_required_for_cents(CENTS_FOR_CANDYBAR);
-    Coins pay_for_candy = pocket.extract_exact_change(requiredChange);
-    cout << "I bought a candy bar for " << CENTS_FOR_CANDYBAR << " cents using " << pay_for_candy << "." << endl;
-    cout << "I have " << pocket << " left in my pocket." << endl;
-	return 0;
-}
 
