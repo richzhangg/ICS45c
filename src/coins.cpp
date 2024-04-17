fc8a49795207e059a490be896aa23f0b80d0d812
Richard Zhang <richarzz@uci.edu>
2024-04-17 14:46:57 -0700

    “something”

--- src/coins.cpp
+++ src/coins.cpp
@@ -1,1 +1,7 @@
+#include "coins.hpp"
 
+#include <iostream>
+
+using namespace std;
+
+// all the definitions for Class 'Coins'
--- src/coins_menu.cpp
+++ src/coins_menu.cpp
@@ -1,1 +1,9 @@
+#include <iostream>
 
+#include "coins.hpp"
+
+using namespace std;
+
+int main() {
+	coins_menu(cin, cout);
+}
