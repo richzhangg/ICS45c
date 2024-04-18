95583f461b16ff6b0bd891c4ac772b6eb6009e7c
Richard Zhang <richarzz@uci.edu>
2024-04-17 23:25:15 -0700

    “something”

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include "word_count.hpp"
+using namespace std;
 
+void to_lowercase(std::string& str) {
+	for (char &c : str) {
+        c = tolower(c);
+    }
+}
+
+std::set<std::string> load_stopwords(std::istream& stopwords) {
+	set<string> sw;
+    string word;
+    while (stopwords >> word) {
+        to_lowercase(word);
+        sw.insert(word);
+    }
+    return sw;
+}
+
+std::map<std::string, int> count_words(std::istream& document, const std::set<std::string>& stopwords) {
+	map<string, int> counts;
+    string token;
+    while (document >> token) {
+        to_lowercase(token);
+        if (!stopwords.count(token)) {
+            counts[token]++;
+        }
+    }
+    return counts;
+}
+
+void output_word_counts(const std::map<std::string, int>& word_counts, std::ostream& output) {
+	for (const auto& pair : word_counts) {
+        output << pair.first << " " << pair.second << endl;
+    }
+}
