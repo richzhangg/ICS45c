e7d52f51809c5ebad7da157bab8d7dacf0299bf1
Richard Zhang <richarzz@uci.edu>
2024-05-01 20:58:29 -0700

    “something”

--- src/string.cpp
+++ src/string.cpp
@@ -1,1 +1,34 @@
+#include "string.hpp"
+#include <iostream>
+using namespace std;
 
+String::String(const char* s) : buf(strdup(s)) {}
+
+String::String(const String &s) : buf(strdup(s.buf) {}
+
+String::String(String &&s) {
+	buf = s.buf;
+	s.buf = nullptr;
+}
+
+void String::swap(String &s) {
+	char* tempo = buf;
+	buf = s.buf;
+	s.buf = tempo;
+}
+
+String &String::operator=(const String& s) {
+	if (this == &s) return *this;
+    if (buf) delete[] buf;
+    buf = strdup(s.buf);
+    return *this;
+}
+
+String &String::operator=(String &&s) {
+	if (this != &s) {
+		delete[] buf;
+		buf = s.buf;
+		s.buf = nullptr;
+	}
+	return *this;
+}
