fe6dd04f8171cb23101b80f24e75ff00c897b76a
Richard Zhang <richarzz@uci.edu>
2024-05-01 23:10:07 -0700

    “something”

--- src/string.cpp
+++ src/string.cpp
@@ -175,8 +175,8 @@ char *String::strdup(const char *src) {
 }
 
 char *String::strncpy(char *dest, const char *src, int n) {
-	int i;
-    for (i = 0; i < n && src[i] != '\0'; ++i) {
+	int i = 0;
+    for (; i < n && src[i] != '\0'; ++i) {
         dest[i] = src[i];
     }
     for (; i < n; ++i) {
