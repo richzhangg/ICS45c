76dbd02bfcd93d2ecce663fc6ee8ff289f9bbdce
Richard Zhang <richarzz@uci.edu>
2024-05-15 16:35:10 -0700

    “something”

--- src/picture.cpp
+++ src/picture.cpp
@@ -1,61 +1,64 @@
 #include "picture.hpp"
 #include <iostream>
-#include <utility>
+
 
 Picture::Picture() : head(nullptr), tail(nullptr) {}
 
-Picture::Picture(const Picture& other) {
-    head = tail = nullptr;
-    if (other.head != nullptr) {
-        ListNode* current = other.head;
-        while (current != nullptr) {
-			ListNode* newNode = new ListNode;
-            newNode->shape = current->shape->clone();
-            if (tail == nullptr) {
-                head = tail = newNode;
-            } else {
-                tail->next = newNode;
-                newNode->prev = tail;
-                tail = newNode;
-            }
-            current = current->next;
-        }
+
+Picture::~Picture() {
+    ListNode* current = head;
+    while (current != nullptr) {
+        ListNode* next = current->next;
+        delete current->shape;
+        delete current;
+        current = next;
     }
 }
+
+
+Picture::Picture(const Picture& other) : head(nullptr), tail(nullptr) {
+    ListNode* current = other.head;
+    while (current != nullptr) {
+        add(*(current->shape));
+        current = current->next;
+    }
+}
+
 
 Picture::Picture(Picture&& other) : head(other.head), tail(other.tail) {
     other.head = nullptr;
     other.tail = nullptr;
 }
 
+
 void Picture::swap(Picture& other) {
-	using std::swap;
+    using std::swap;
     swap(head, other.head);
     swap(tail, other.tail);
 }
 
+
 Picture& Picture::operator=(const Picture& other) {
-    Picture curr(other);
-    swap(curr);
+    Picture temp(other);
+    swap(temp);
     return *this;
 }
+
 
 Picture& Picture::operator=(Picture&& other) {
-	swap(other);
+    swap(other);
     return *this;
 }
 
+
 void Picture::add(const Shape& shape) {
-    ListNode *newEntry = new ListNode;
-    newEntry->shape = shape.clone();
-    newEntry->next = head;
-    if (head == nullptr) {
-        tail = newEntry;
+    ListNode* newNode = new ListNode{shape.clone(), nullptr};
+    if (tail) {
+        tail->next = newNode;
     } else {
-        head->prev = newEntry;
+        head = newNode;
     }
-    head = newEntry;
-    newEntry->prev = nullptr;
+    tail = newNode;
 }
 
 
@@ -88,11 +91,3 @@ double Picture::total_area() const {
     return total;
 }
 
-Picture::~Picture() {
-	for (ListNode* current = head; current != nullptr; ) {
-        ListNode* next = current->next;
-        delete current->shape;
-        delete current;
-        current = next;
-    }
-}
