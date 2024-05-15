eed8aaeacb73d8a21e8c387cf2589044a9333e09
Richard Zhang <richarzz@uci.edu>
2024-05-15 12:41:43 -0700

    “something”

--- src/shape.cpp
+++ src/shape.cpp
@@ -2,7 +2,10 @@
 #include <iostream>
 using namespace std;
 
-Shape::Shape(Point center, string name) : center(center), name(name) {}
+Shape::Shape(Point center, string name) {
+    this->center = center;
+    this->name = name;
+}
 
 void Shape::print(ostream& out) const {
     out << name << " at (" << center.x << ", " << center.y << ") area = " << this->area() << endl;
